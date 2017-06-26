7886d1216f7d54a6a7bf4d413b3781a3a8a17e43
Rui Paiva Branco <email@ruipaivabranco.com>
2017-06-27 00:38:33 +0100

    Update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
 
- $("#main").append('ruipaivabranco');
+$("#main").append('ruipaivabranco');
+
+var name = "Rui Paiva Branco"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role ="Full Stack Web Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
